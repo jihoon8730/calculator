fd7ae5856e102d504521f45f118134f986477f28
shinjihoon <jihoon.shin@teoul.com>
2022-09-15 15:18:31 +0900

    ADD : 연속 연산 기능 추가 중

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
 import './App.css';
-import { useCallback, useEffect, useState } from 'react';
+import {  useEffect, useState } from 'react';
 
 function App() {
 
-  //debouncing 예제 코드 작성 중
-  const useDebouncedEffect = (func, delay, deps) => {
-    const callback = useCallback(func, deps);
-
-    useEffect(() => {
-      const timer = setTimeout(() => {
-        callback();
-      }, delay);
-
-      return () => {
-        clearTimeout(timer);
-      };
-    }, [callback, delay]);
-  };
-
-  const [numOne, setNumOne] = useState([]);
-  const [numTwo, setNumTwo] = useState([]);
+  const [numOne, setNumOne] = useState('');
+  const [numTwo, setNumTwo] = useState('');
   // State에 버튼1 클릭하면 numOwn배열안에 1값이 push
   // 버튼1 클릭하면 numOwn배열안에 1값이 push
   // 버튼2 클릭하면 numOwn배열안에 2값이 push
@@ -49,23 +34,46 @@ function App() {
   }
 
   const handleOperator = () => {
-    if ((operator === undefined) || (operator === '')) {
-      alert('연산기호를 넣어주세요');
-      setNumOne('');
-      return false;
-    } else if (operator === '+') {
-      return setResult(Math.round((parseFloat(numOne) + parseFloat(numTwo)) * 100) / 100);
+    if (operator === '+') {
+      setResult(Math.round((parseFloat(numOne) + parseFloat(numTwo)) * 100) / 100);
     } else if (operator === '-') {
-      return setResult(Math.round((parseFloat(numOne) - parseFloat(numTwo)) * 100) / 100);
+      setResult(Math.round((parseFloat(numOne) - parseFloat(numTwo)) * 100) / 100);
     } else if (operator === 'X') {
-      return setResult(parseFloat(numOne) * parseFloat(numTwo));
+      setResult(parseFloat(numOne) * parseFloat(numTwo));
     } else if (operator === '/') {
-      return setResult((parseFloat(numOne) / parseFloat(numTwo)).toFixed(2));
-    } 
+      setResult((parseFloat(numOne) / parseFloat(numTwo)).toFixed(2));
+    }
+      setNumOne(result);
+      setOperator('');
+      setNumTwo(''); 
   }
+  console.log("numOne: ", numOne, "numTwo: ", numTwo);
+  console.log("result: ",result);
+  
+  
+  
+  
+
+  // 계산식 여러번 작성하기 순서도
+  // 계산순서 (숫자 => 연산자 => 숫자 => 연산자 => 결과값 => 숫자 => 연산자 => 결과값 => 숫자 => 연산자 => 결과값)
+  // 1. 숫자 버튼을 클릭하면 숫자의 value를 state에 담기
+  // 2. 연산자 클릭시 연산자 value를 state에 담기
+  // 3. 숫자 클릭시 숫자의 value를 숫자2 state에 담기
+  // 4. 연산자 클릭시 숫자1 + 숫자2 결과값 result state에 담기
+
+
+  //debouncing 예제 코드 작성 중
+  //   useEffect(() => {
+  //     let timer = setTimeout(() => {
+  //       onClickNumber('.');
+  //     }, 300);
+  //     return () => {clearTimeout(timer)}
+  //   }, [])
+  // }
+  
 
   // 키보드 숫자패드 입력하기
-  // 순서 정리
+  // ------순서 정리-----
   // 1. 키보드를 누를 때
   // - 키보드 넘버 1을 누르면 onClickNumber('1')이 들어간다
   // - 키보드 넘버 2을 누르면 onClickNumber('2')이 들어간다
@@ -74,7 +82,7 @@ function App() {
   // - key와 관련된 이벤트 사용(onkeydown, onkeyup 등등) 
   // - keyboard를 눌렀을때 keyvalue를 가져 올 수 있는 메소드 사용 = event.key를 사용하여 onkeyDown을 했을 때 내가 누른 key의 value 값을 가져옴
   // - console.log로 확인
-  // 해결 방안
+  // -----해결 방안------
   // 1. 계산기 창에 클릭 했을 때 event key value를 가져온다
   // 2. 가져온 key value를 isFinite()를 통해 숫자만 추출 검사한다
   // 3. e.key가 숫자일 경우 onClickNumber(e.key)를, 그 외의 경우 false
@@ -86,6 +94,7 @@ function App() {
   // 소수점이 하나만 찍혀야 하지만 현제 버튼을 클릭할때마다 눌리고 있다
   // 해결하기 위해 cto님 trottleling, debouncing개념의 키워드를 주셨다
   // 현재 접목해야할 개념으로는 debouncing(디바운싱)
+
   const handleKeyboardOperator = (e) => {
     if (isFinite(e.key)) {
       return onClickNumber(e.key);
@@ -105,8 +114,6 @@ function App() {
       return onClickNumber(e.key);
     }
   }
-  console.log(numOne, numTwo);
-  console.log(result);
   return (
     <div className='wrap' onKeyDown={handleKeyboardOperator}>
       <div className='number-view'>
@@ -157,7 +164,7 @@ function App() {
         }}>+</button>
       </div>
       <div className='numbers'>
-        <button className='number-btn orange-color' onClick={onClickClear}>C</button>
+        <button className='number-btn orange-color' onClick={onClickClear}>AC</button>
         <button className='number-btn' onClick={() => {
           onClickNumber('0');
         }}>0</button>
@@ -168,7 +175,9 @@ function App() {
         }}>/</button>
       </div>
       <div className='numbers'>
-      <button className='number-btn' onClick={useDebouncedEffect}>.</button>
+      <button className='number-btn-dot' onClick={() => {
+        onClickNumber('.');
+      }}>.</button>
       </div>
     </div>
   );
